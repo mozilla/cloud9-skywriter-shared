bb8de7b479cddd5d59dce7f6d60500547484a6ae
Joe Walker <jwalker@mozilla.com>
2010-11-10 14:38:08 +0000

    convert oop to commonjs export format

--- plugins/ace/lib/ace/mode/html.js
+++ plugins/ace/lib/ace/mode/html.js
@@ -37,7 +37,7 @@
 
 define(function(require, exports, module) {
 
-var oop = require("ace/lib/oop");
+var oop = require("ace/lib/oop").oop;
 var TextMode = require("ace/mode/text");
 var JavaScriptMode = require("ace/mode/javascript");
 var CssMode = require("ace/mode/css");
